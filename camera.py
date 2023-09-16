7ae9be4b8ef94b60fbd8614f82ef37238b3ad8f2
JHadwin1 <jeremiahhadwin@yahoo.com>
2023-09-16 16:29:22 -0400

    Updated camera.py efficiency

--- camera.py
+++ camera.py
@@ -4,28 +4,18 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-from split_image import split_image
 
 
-# Note camera IDs may differ based upon hardware
-camL = cv2.VideoCapture(1)
-if not camL.isOpened():
- print("Cannot open left camera")
- exit()
-camR = cv2.VideoCapture(0)
-if not camR.isOpened():
- print("Cannot open right camera")
- exit()
-# Read frames from each camera
-retL, frameL = camL.read()
-if not retL:
-    print("Can't receive frame from left camera")
-retR, frameR = camR.read()
-if not retR:
-    print("Can't recieve frame from right camera")
+vid = cv2.VideoCapture('data/vr-vid.mp4')
+if (vid.isOpened()== False): 
+  print("Error opening video stream or file")
+
+
+#out = cv2.VideoWriter('data/outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 30, (960,1080))
+
 
 index = 0
-stereo = cv2.StereoBM_create(numDisparities=272, blockSize=31)
+stereo = cv2.StereoBM_create(numDisparities=64, blockSize=21)
 stereo.setPreFilterType(1)
 stereo.setPreFilterSize(7)
 stereo.setPreFilterCap(1)
@@ -33,28 +23,36 @@ stereo.setTextureThreshold(10)
 stereo.setUniquenessRatio(5)
 stereo.setSpeckleRange(0)
 stereo.setSpeckleWindowSize(0)
-stereo.setDisp12MaxDiff(10)
+stereo.setDisp12MaxDiff(0)
 stereo.setMinDisparity(0)
-while retL == True & retR == True:
+
+ret, rawFrame = vid.read()
+
+#cv2.imwrite('data/rawFrame.png', rawFrame)
+
+#split_image('data/rawFrame.png', 1, 2, False, False)
+framesRead = 0
+while ret == True:
+    lArray, rArray = np.hsplit(rawFrame, 2)
+    framesRead += 1
     # Convert frames to grayscale images
-    stereoL = cv2.cvtColor(frameL, cv2.COLOR_BGR2GRAY) 
-    stereoR = cv2.cvtColor(frameR, cv2.COLOR_BGR2GRAY) 
-    res720 = (1280, 720)
-    stereoL = cv2.resize(stereoL, res720)
-    stereoR = cv2.resize(stereoR, res720)
+
+
+    stereoL = cv2.cvtColor(lArray, cv2.COLOR_BGR2GRAY) 
+    stereoR = cv2.cvtColor(rArray, cv2.COLOR_BGR2GRAY) 
+
 
 
     disparity = stereo.compute(stereoL, stereoR)
     cv2.imwrite('data/frame.png', disparity)
-    newRes = (6, 6)
+    #scaledDisparity = cv2.resize(disparity, (960, 1080))
+    #out.write(scaledDisparity)
+    newRes = (8, 8)
     resized = cv2.resize(disparity, newRes)
     print(resized)
+    print(framesRead)
     cv2.imwrite('data/resizedFrame.png',resized)
-    retL, frameL = camL.read()
-    retR, frameR = camR.read()
-    if not retL:
-        print("Can't receive frame from left camera")
-        break
-    if not retR:
-        print("Can't recieve frame from right camera")
-        break
+    ret, rawFrame = vid.read()
+
+vid.release
+out.release
